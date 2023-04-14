4f59bcdd7c41006dc2d3e5a5079d038130b0cf48
omgate <90276220+OMGATE23@users.noreply.github.com>
2023-04-14 21:00:14 +0530

    server : connected to database

--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const app = express()
 require('dotenv').config()
+const connectToDB = require('./config/db')
 
+connectToDB()
+app.use(express.json())
 
 app.listen(process.env.PORT, () => {
     console.log("Server is running on port" , process.env.PORT);
